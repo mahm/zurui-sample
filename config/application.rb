448c280136652570817b060073f8fab4e558b591
mahm <nishimi@sonicgarden.jp>
2012-11-07 14:13:00 +0900

    Add option to application.rb

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module CustomizedTwitterBootstrap
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Don't initialize on precompile
+    config.assets.initialize_on_precompile = false
   end
 end
